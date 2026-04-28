4efc537ed4c2df1363bf2d884094f82033ea13ea
talnpoika <mikko.talonpoika@student.lut.fi>
2026-04-28 19:15:34 +0300

    updated version news

--- server/news-microservice/newsServer.py
+++ server/news-microservice/newsServer.py
@@ -1,19 +1,51 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
 import requests
+import json
 
-def getNews(topic):
-    
-    url = ('https://newsapi.org/v2/everything?'
-           'q={topic}&'
-           'sortBy=popularity&'
-           'pageSize=10&'
-           'language=en&'
-           'apiKey=1b5bb8dd11604a898ca733c287980a53'
-           )
-    
-    response = requests.get(url)
-    content = response.json()
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2')
 
-    return content
+with SimpleXMLRPCServer(('localhost', 8000),
+                        requestHandler=RequestHandler) as server:
+    server.register_introspection_functions
+
+    def getNews(topic):
+        
+        url = ('https://newsapi.org/v2/everything?'
+            'q={topic}&'
+            'sortBy=popularity&'
+            'pageSize=10&'
+            'language=en&'
+            'apiKey=1b5bb8dd11604a898ca733c287980a53'
+            )
+        
+        response = requests.get(url)
+        content = response.json()
+
+        newslist = []
+
+        for article in content['articles']:
+            name = article['source']['name']
+            title = article['title']
+            description = article['description']
+            time = article['publishedAt']
+
+            news = {
+                  "source": name,
+                  "title": title,
+                  "description": description,
+                  "publishTime": time
+            }
+            
+            newslist.append(json.dumps(news))
+
+        return newslist
+
+    server.register_function(getNews, 'getNews')
+
+    server.serve_forever()
 
 if __name__ == '__main__':
-    getNews('tesla')
+    RequestHandler()
+    
